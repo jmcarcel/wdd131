2a3d69a647725907b0dab1c937f1705f8f1783d1
jmcarcel <carceljoan@gmail.com>
2025-05-24 01:12:09 -0300

    bom js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,20 @@
-const input = document.querySelector("#favchap");
-const button = document.querySelector("button");
-const list = document.querySelector("#list");
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
 li.textContent = input.value;
 deleteButton.textContent = "❌";
 li.append(deleteButton);
 list.append(li);
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+    
+    }
+});
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
+input.value = '';
+input.focus();
